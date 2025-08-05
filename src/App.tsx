a1b76f6d2c98df0825b8452c37d5ed89cd4e4538
django228 <d123kbudim@gmail.com>
2025-08-05 18:05:31 +0300

    Adding theme changing, adding light-theme and dark-theme. Adding useTheme() hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React, {Suspense} from 'react';
-import './index.scss'
+import React, {Suspense} from "react";
+import './styles/index.scss';
 import {Route, Routes, Link} from "react-router-dom";
 
 import Counter from "./components/Counter";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Button} from "@storybook/react/demo";
+import {useTheme} from "./hooks/useTheme";
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Link to={'/counter'}>Счётчик</Link>
+        <div className={`app ${theme}`}>
+            <Button onClick={toggleTheme}>theme</Button>
+            <br></br>
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О сайте</Link>
+            <Link to={"/counter"}>Счётчик</Link>
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={"/about"} element={<AboutPageAsync />} />
-                    <Route path={"/"} element={<MainPageAsync />} />
-                    <Route path={"/counter"} element={<Counter />} />
+                    <Route path={"/about"} element={<AboutPageAsync/>}/>
+                    <Route path={"/"} element={<MainPageAsync/>}/>
+                    <Route path={"/counter"} element={<Counter/>}/>
                 </Routes>
             </Suspense>
         </div>
